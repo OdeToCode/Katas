1409f7fdb927745635c8d742705a210150d75fb6
mark-breen <markbreen42@gmail.com>
2015-02-21 15:25:28 +0000

    Refactored tests

--- Refactoring/CS/Algorithm.Tests/FinderTests.cs
+++ Refactoring/CS/Algorithm.Tests/FinderTests.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using Xunit;
+using NUnit.Framework;
 
 namespace Algorithm.Test
 {    
+    [TestFixture]
     public class FinderTests
     {
-        [Fact]
+        Thing sue = new Thing() { Name = "Sue", BirthDate = new DateTime(1950, 1, 1) };
+        Thing greg = new Thing() { Name = "Greg", BirthDate = new DateTime(1952, 6, 1) };
+        Thing sarah = new Thing() { Name = "Sarah", BirthDate = new DateTime(1982, 1, 1) };
+        Thing mike = new Thing() { Name = "Mike", BirthDate = new DateTime(1979, 1, 1) };
+
+        [Test]
         public void Returns_Empty_Results_When_Given_Empty_List()
         {
             var list = new List<Thing>();
@@ -18,7 +24,7 @@ namespace Algorithm.Test
             Assert.Null(result.P2);
         }
 
-        [Fact]
+        [Test]
         public void Returns_Empty_Results_When_Given_One_Person()
         {
             var list = new List<Thing>() { sue };
@@ -26,11 +32,11 @@ namespace Algorithm.Test
 
             var result = finder.Find(FT.One);
 
-            Assert.Null(result.P1);
-            Assert.Null(result.P2);
+            Assert.That(result.P1, Is.Null);
+            Assert.That(result.P2, Is.Null);
         }
 
-        [Fact]
+        [Test]
         public void Returns_Closest_Two_For_Two_People()
         {
             var list = new List<Thing>() { sue, greg };
@@ -38,11 +44,11 @@ namespace Algorithm.Test
 
             var result = finder.Find(FT.One);
 
-            Assert.Same(sue, result.P1);
-            Assert.Same(greg, result.P2);
+            Assert.That(sue, Is.SameAs(result.P1));
+            Assert.That(greg, Is.SameAs(result.P2));
         }
 
-        [Fact]
+        [Test]
         public void Returns_Furthest_Two_For_Two_People()
         {
             var list = new List<Thing>() { greg, mike };
@@ -50,11 +56,11 @@ namespace Algorithm.Test
 
             var result = finder.Find(FT.Two);
 
-            Assert.Same(greg, result.P1);
-            Assert.Same(mike, result.P2);
+            Assert.That(greg, Is.SameAs(result.P1));
+            Assert.That(mike, Is.SameAs(result.P2));
         }
 
-        [Fact]
+        [Test]
         public void Returns_Furthest_Two_For_Four_People()
         {
             var list = new List<Thing>() { greg, mike, sarah, sue };
@@ -62,11 +68,11 @@ namespace Algorithm.Test
 
             var result = finder.Find(FT.Two);
 
-            Assert.Same(sue, result.P1);
-            Assert.Same(sarah, result.P2);
+            Assert.That(sue, Is.SameAs(result.P1));
+            Assert.That(sarah, Is.SameAs(result.P2));
         }
 
-        [Fact]
+        [Test]
         public void Returns_Closest_Two_For_Four_People()
         {
             var list = new List<Thing>() { greg, mike, sarah, sue };
@@ -74,13 +80,9 @@ namespace Algorithm.Test
 
             var result = finder.Find(FT.One);
 
-            Assert.Same(sue, result.P1);
-            Assert.Same(greg, result.P2);
+            Assert.That(sue, Is.SameAs(result.P1));
+            Assert.That(greg, Is.SameAs(result.P2));
         }
 
-        Thing sue = new Thing() {Name = "Sue", BirthDate = new DateTime(1950, 1, 1)};
-        Thing greg = new Thing() {Name = "Greg", BirthDate = new DateTime(1952, 6, 1)};
-        Thing sarah = new Thing() { Name = "Sarah", BirthDate = new DateTime(1982, 1, 1) };
-        Thing mike = new Thing() { Name = "Mike", BirthDate = new DateTime(1979, 1, 1) };
     }
 }
